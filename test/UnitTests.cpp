8c542cf729121e74151aa07f6387268368b23772
Ivan Habernal <ivan.habernal@tu-darmstadt.de>
2023-07-28 17:08:25 +0200

    Adding fmt to unit test

--- test/UnitTests.cpp
+++ test/UnitTests.cpp
@@ -1,8 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
 #include "HelloWorld.h"
+#include <fmt/core.h>
 
 TEST_CASE("Hello world") {
     auto dummy_class{HelloWorld{}};
+    fmt::print("Test");
 
     REQUIRE(123 == dummy_class.GetValue());
 };
